0eb761e40ceb9d147b44383465eef72d32ad3676
niyam007 <niyam.bhat@computingaustralia.group>
2022-01-26 13:50:22 +0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Tweet from "./components/Tweet";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://coursera-twitter-api.herokuapp.com/tweets"
+      );
+      setData(response.data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: "#111111", padding: "30px" }}>
+      {data.map((item) => (
+        <Tweet
+          id={item.id}
+          author={item.author}
+          date={item.date}
+          text={item.text}
+        />
+      ))}
     </div>
   );
 }
